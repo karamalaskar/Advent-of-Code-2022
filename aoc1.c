b618af5480560de14484b8a34669e4ef683a4029
karamalaskar <93408430+karamalaskar@users.noreply.github.com>
2022-12-30 19:42:42 +0100

    Day 1

---
+++ aoc1.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+    FILE *f = fopen("aoc1.txt", "r");
+
+    char line[10];
+
+    int elf_count = 0;
+
+    while (fgets(line, sizeof(line), f))
+        if (strlen(line) == 1)
+            elf_count++;
+
+    fclose(f);
+
+    int elves[elf_count];
+
+    for (int i = 0; i < elf_count; i++)
+        elves[i] = 0;
+
+    int calories;
+    int elf = 0;
+
+    f = fopen("aoc1.txt", "r");
+
+    while (fgets(line, sizeof(line), f))
+    {
+
+        calories = atoi(line);
+
+        if (strlen(line) != 1)
+            elves[elf] += calories;
+        else
+            elf++;
+    }
+
+    int max = 0;
+
+    for (int i = 0; i < elf_count; i++)
+        if (elves[i] > elves[max])
+            max = i;
+
+    printf("Elf #%i is carrying the most calories (%i).\n", max + 1, elves[max]);
+
+    fclose(f);
+}
